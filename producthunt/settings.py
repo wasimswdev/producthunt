6e12285fe3ee322366fd47f0c362625f9d0f3218
Wasim_CVE <38834782+wasimswdev@users.noreply.github.com>
2020-02-12 13:53:26 +0530

    changes in settings

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -68,9 +68,16 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default':   {
+        'ENGINE': 'django.db.backends.sqlite3_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }                               #dj_database_url.config()
 
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
